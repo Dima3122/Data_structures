6cf5b3875029ec3d878e8fb5cddc6046afa7940f
Дмитрий Савостин <dimasavostin075@gmail.com>
2022-02-01 19:33:37 +0700

    Add InsertonSort

---
+++ Sort.Bl/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort.Bl
+{
+    public class InsertionSort<T> : AlgorithmBase<T> where T: IComparable
+    {
+        protected override void MakeSort()
+        {
+            for (int i = 1; i < Items.Count; i++)
+            {
+                var temp = Items[i];
+                var j = i;
+                while (j > 0 && temp.CompareTo(Items[j - 1]) == -1)
+                {
+                    Items[j] = Items[j - 1];
+                    j--;
+                    SwopCount++;
+                    СompareCount++;
+                }
+                Items[j] = temp;
+            }     
+        }
+    }
+}
---
+++ Sort.BlTests/InsertSortTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Sort.Bl.Tests
+{
+    [TestClass()]
+    public class InsertSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            var cocktailSort = new InsertionSort<int>();
+            var rnd = new Random();
+            var items = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+            cocktailSort.Items.AddRange(items);
+            items.Sort();
+            cocktailSort.Sort();
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(items[i], cocktailSort.Items[i]);
+            }
+        }
+    }
+}
